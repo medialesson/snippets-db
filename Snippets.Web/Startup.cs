a7523da07b981a98f3a2cb5b02bdd098e0d54185
Gino Messmer <g.messmer@ftsoftware.eu>
2018-10-11 16:32:34 +0200

    Revert "Merge branch 'feature/users' into feature/auth"
    This reverts commit 313b46e5d0b6e6f94428f0fe6b9e6704ff2df449, reversing
    changes made to d085682aa31ed09a481975661d6759d4dde2939d.

--- Snippets.Web/Startup.cs
+++ Snippets.Web/Startup.cs
@@ -1,13 +1,10 @@
-﻿using AutoMapper;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -16,7 +13,6 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using NSwag.AspNetCore;
 using Snippets.Web.Common.Database;
-using Snippets.Web.Common.Security;
 
 namespace Snippets.Web
 {
@@ -32,8 +28,6 @@ namespace Snippets.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add MediatR and database services
-            services.AddMediatR();
             services.AddEntityFrameworkSqlite().AddDbContext<SnippetsContext>(options =>
             {
                 options.UseSqlite("Data Source=snippets.db");
@@ -41,16 +35,6 @@ namespace Snippets.Web
 
             services.BuildServiceProvider().GetRequiredService<SnippetsContext>().Database.EnsureCreated();
 
-            // Add auto mapper
-            services.AddAutoMapper(GetType().Assembly);
-
-
-            // Add common services
-            services.AddScoped<IPasswordHasher, PasswordHasher>();
-            services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
-
-
-            // Add MVC
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddSwagger();
         }
