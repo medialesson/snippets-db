1c87a7d207822bc399c668919e9abb481e686e6c
Samuel Oechsler <samuel@boxnetwork.de>
2018-10-11 14:46:57 +0200

    Add user accessor helper

---
+++ Snippets.Web/Common/CurrentUserAccessor.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+
+namespace Snippets.Web.Common
+{
+    public class CurrentUserAccessor : ICurrentUserAccessor
+    {
+        readonly IHttpContextAccessor _httpContextAccessor;
+
+        public CurrentUserAccessor(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string GetCurrentUsername()
+        {
+            return _httpContextAccessor.HttpContext.User?.Claims
+                ?.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+        }
+    }
+}
