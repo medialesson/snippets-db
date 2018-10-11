00577c92d0e5d5adcd75dd097a10e6b99132f3ac
Samuel Oechsler <oechsler@medialesson.de>
2018-10-11 13:35:57 +0200

    Add auth helpers

--- Snippets.Web/Common/Security/JwtTokenGenerator.cs
+++ Snippets.Web/Common/Security/JwtTokenGenerator.cs
@@ -1,7 +1,40 @@
-﻿namespace Snippets.Web.Common.Security
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+
+namespace Snippets.Web.Common.Security
 {
-    public class JwtTokenGenerator
+    public class JwtTokenGenerator : IJwtTokenGenerator
     {
-        
+        private readonly JwtIssuerOptions _jwtOptions;
+
+        public JwtTokenGenerator(IOptions<JwtIssuerOptions> jwtOptions)
+        {
+            _jwtOptions = jwtOptions.Value;
+        }
+
+        public async Task<string> CreateToken(string userId)
+        {
+            var claims = new Claim[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, userId),
+                new Claim(JwtRegisteredClaimNames.Jti, await _jwtOptions.JtiGenerator()),
+                new Claim(JwtRegisteredClaimNames.Iat,
+                new DateTimeOffset(_jwtOptions.IssuedAt).ToUnixTimeSeconds().ToString(), ClaimValueTypes.Integer64),
+            };
+            var jwt = new JwtSecurityToken(
+                _jwtOptions.Issuer,
+                _jwtOptions.Audience,
+                claims,
+                _jwtOptions.NotBefore,
+                _jwtOptions.Expiration,
+                _jwtOptions.SigningCredentials
+            );
+
+            
+            return new JwtSecurityTokenHandler().WriteToken(jwt);
+        }
     }
 }
