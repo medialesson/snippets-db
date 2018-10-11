47a4eacf1613878ba09cfe0257d8d2840d7ad16e
Samuel Oechsler <oechsler@medialesson.de>
2018-10-11 13:35:57 +0200

    Add auth helpers

---
+++ Snippets.Web/Common/Security/IPasswordHasher.cs
@@ -0,0 +1,7 @@
+﻿namespace Snippets.Web.Common.Security
+{
+    public interface IPasswordHasher
+    {
+        byte[] Hash(string password, byte[] salt);
+    }
+}
---
+++ Snippets.Web/Common/Security/PasswordHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Snippets.Web.Common.Security
+{
+    public class PasswordHandler : IPasswordHasher
+    {
+        readonly HMACSHA512 _algorithm; 
+
+        public PasswordHandler(string secret)
+        {
+           _algorithm = new HMACSHA512(Encoding.UTF8.GetBytes(secret));
+        }
+
+        public byte[] Hash(string password, byte[] salt)
+        {
+            var bytes = Encoding.UTF8.GetBytes(password);
+
+            var allBytes = new byte[bytes.Length + salt.Length];
+            Buffer.BlockCopy(bytes, 0, allBytes, 0, bytes.Length);
+            Buffer.BlockCopy(salt, 0, allBytes, bytes.Length, salt.Length);
+
+            return _algorithm.ComputeHash(allBytes);
+        }
+    }
+}
