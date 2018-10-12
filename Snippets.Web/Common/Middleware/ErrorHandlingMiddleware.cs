0ddc6928f5fea94db6ab0c59c1dd1b045253577a
Gino Messmer <g.messmer@ftsoftware.eu>
2018-10-12 14:17:40 +0200

    Add exception handler middleware

---
+++ Snippets.Web/Common/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Localization;
+using Newtonsoft.Json;
+using Snippets.Web.Common.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Snippets.Web.Common.Middleware
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ErrorHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch(Exception ex)
+            {
+                await HandleExceptionAsync(context, ex); // TODO: Add logger
+            }
+        }
+
+        public static async Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            object errors = null;
+
+            switch (exception)
+            {
+                case RestException re:
+                    errors = re.Errors;
+                    context.Response.StatusCode = (int)re.Code;
+                    break;
+
+                case Exception ex:
+                    errors = string.IsNullOrWhiteSpace(ex.Message) ? "Error" : ex.Message;
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    break;
+            }
+
+            context.Response.ContentType = "application/json";
+
+            var result = JsonConvert.SerializeObject(new
+            {
+                errors
+            });
+
+            await context.Response.WriteAsync(result);
+        }
+    }
+}
