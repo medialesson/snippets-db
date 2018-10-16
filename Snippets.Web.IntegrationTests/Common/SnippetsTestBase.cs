1ac4409dfcb3cd9ce62ea3396e0a0dcb1f65e2dc
Gino Messmer <g.messmer@ftsoftware.eu>
2018-10-16 16:12:24 +0200

    Add snippets test base class

--- Snippets.Web.IntegrationTests/Common/SnippetsTestBase.cs
+++ Snippets.Web.IntegrationTests/Common/SnippetsTestBase.cs
@@ -1,10 +1,41 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.DependencyInjection;
+using Snippets.Web.Common.Database;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace Snippets.Web.IntegrationTests.Common
 {
-    public class SnippetsTestBase
+    public class SnippetsTestBase : IDisposable
     {
+        private IServiceScopeFactory _serviceScopeFactory;
+        private string _dbName = $"{Guid.NewGuid().ToString()}.db";
+
+        public SnippetsTestBase()
+        {
+            var configBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.Testing.json");
+
+            var startup = new Startup(configBuilder.Build());
+            var services = new ServiceCollection();
+
+            services.AddDbContext<SnippetsContext>(options => options.UseSqlite(_dbName));
+
+            startup.ConfigureServices(services);
+            var serviceProvider = services.BuildServiceProvider();
+
+            serviceProvider.GetRequiredService<SnippetsContext>().Database.EnsureCreated();
+            _serviceScopeFactory = serviceProvider.GetService<IServiceScopeFactory>();
+        }
+
+        public void Dispose()
+        {
+            File.Delete(_dbName);
+        }
     }
 }
